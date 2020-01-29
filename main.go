1fa074f9b492531056f97529caa24fa30be9ddb6
PumpkinSeed <qwer.kocka@gmail.com>
2020-01-29 14:47:38 +0100

    Add next service

--- main.go
+++ main.go
@@ -3,34 +3,59 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-)
-
-const (
-	port = 8080
+	"os"
+	"strings"
 )
 
 var (
 	serviceid string
+	next string
+	port string
 )
 
 func init() {
 	serviceid = pseudoUUID()
+	next = os.Getenv("SS_NEXT")
+	port = os.Getenv("SS_PORT")
+	if port == "" {
+		port = "8080"
+	}
 }
 
 func main() {
 	http.HandleFunc("/", serviceidHandler)
 	log.Printf("Server listening on %s", getPort())
+	if next != "" {
+		log.Printf("Next service: %s", next)
+	}
 	http.ListenAndServe(getPort(), nil)
 }
 
 func serviceidHandler(w http.ResponseWriter, r *http.Request) {
+	var serviceStack []string
+	if next != "" {
+		resp, err := http.Get(next)
+		if err != nil {
+			panic(err)
+		}
+		respbytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		serviceStack = append(serviceStack, serviceid)
+		result := strings.Split(string(respbytes), ",")
+		serviceStack = append(serviceStack, result...)
+		fmt.Fprintf(w, strings.Join(serviceStack, ","))
+		return
+	}
 	fmt.Fprintf(w, serviceid)
 }
 
 func getPort() string {
-	return fmt.Sprintf(":%d", port)
+	return fmt.Sprintf(":%s", port)
 }
 
 func pseudoUUID() (uuid string) {
